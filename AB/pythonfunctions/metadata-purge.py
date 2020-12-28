3376fa35b5ad646a4e44d66e84cdc7c34b2587af
Patrick Duffy <patrickjosephduffy@me.com>
2020-12-28 14:21:24 -0800

    fixed search

---
+++ AB/pythonfunctions/metadata-purge.py
@@ -0,0 +1,17 @@
+from decimal import Decimal
+
+import boto3
+import os
+import json
+
+DYNAMODB_TABLE = os.environ.get("DYNAMODB_TABLE", None)
+dynamodb = boto3.resource("dynamodb")
+dynamodb_table = dynamodb.Table(DYNAMODB_TABLE)
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        key = record['s3']['object']['key']
+
+        dynamodb_table.delete_item(Key={
+            "ImageId": key,
+        })
